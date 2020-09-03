a74211f4974183597b1689a89a6eaec4c44ebfce
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-03 10:58:52 +0300

    Add HashTable manual implementation w/ array and linked list

---
+++ DataStructuresPractice/DataStructuresPractice/HashMap/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresPractice.HashMap
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public string Key { get; set; }
+        public T Value { get; set; }
+
+    }
+}
---
+++ DataStructuresPractice/DataStructuresPractice/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DataStructuresPractice.HashMap;
+
+namespace DataStructuresPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region HashTable HashMap Dictionary manual implementation
+            // HashTable (HashMap, Dictionary)
+
+            var h = new DataStructuresPractice.HashMap.HashTable<int>(4);
+
+            Console.WriteLine(h.GetBucketByKey("One"));
+            Console.WriteLine(h.GetBucketByKey("Two"));
+            Console.WriteLine(h.GetBucketByKey("Three"));
+
+            h.Add("One", 1);
+            h.Add("Two", 2);
+            h.Add("Three", 3);
+
+            Console.WriteLine(h.Get("One"));
+            Console.WriteLine(h.Get("Two"));
+            Console.WriteLine(h.Get("Three"));
+
+            try
+            {
+                Console.WriteLine(h.Get("Four"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(h.Remove("Three"));
+            Console.WriteLine(h.Remove("Three"));
+            Console.WriteLine(h.Remove("Two"));
+            Console.WriteLine(h.Remove("Two"));
+            #endregion
+        }
+    }
+}
